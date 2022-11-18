cba1bda5a24b7c2a55c355e987e86f4a97ecac95
profit0124 <profit0124@gmail.com>
2022-11-18 12:28:30 +0900

    aaa
    a

---
+++ Minigames/Minigames/MinigamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinigamesApp.swift
+//  Minigames
+//
+//  Created by Sooik Kim on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct MinigamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
